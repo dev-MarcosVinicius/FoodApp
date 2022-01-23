2e5cc85003f3a15953e98b246273da472a9174a6
Marcos Vinicius <marcosoliveira.ti9@gmail.com>
2022-01-23 13:47:13 -0300

    Configurations of fonts

--- App.tsx
+++ App.tsx
@@ -2,12 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
 import { NavigationContainer } from '@react-navigation/native';
 
 import theme from './src/global/styles/theme';
 import { Background } from './src/components/Background';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading/>
+  }
+
   return (
     <NavigationContainer>
       <ThemeProvider theme={theme}>
