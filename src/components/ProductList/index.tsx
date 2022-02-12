b58fb1f8bdc556cc709b29fcef4cc6ee699e7c6e
Marcos Vinicius <marcosoliveira.ti9@gmail.com>
2022-02-12 12:01:50 -0300

    Optimizing list

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -29,56 +29,53 @@ export function ProductList({data, onProductSelected}: ListProductProps) {
 
     return (
         <Container>
-            {
-                data.length <= 0
-                ?
-                <ZeroProducts>
-                    Não foi possivel encontrar nenhum produto.
-                </ZeroProducts>
-                :
-                <List
-                    data={data}
-                    keyExtractor={item => item.id}
-                    ItemSeparatorComponent={() => (
-                        <SeparatorList/>
-                    )}
-                    renderItem={({item}) => (
-                        <Item
-                            onPress={() => handleSelectCategory(item)}
-                        >
-                            <Content>
-                                <ImageProduct
-                                    source={{uri: item.imageUrl}}
-                                    resizeMode="cover"
-                                />
-                                
-                                <Fields>
-                                    <SuperiorFields>
-                                        <Title>
-                                            {item.title}
-                                        </Title>
-                                    </SuperiorFields>
-    
-                                    <MiddleFields>
-                                        <SubTitle>
-                                            {StringFormat.substring(item.description, 50)}
-                                        </SubTitle>
-                                    </MiddleFields>
-    
-                                    <InferiorFields>
-                                        <PriceField>
-                                            <IconPrice/>
-                                            <Price>
-                                                {item.price}
-                                            </Price>
-                                        </PriceField>
-                                    </InferiorFields>
-                                </Fields>
-                            </Content>
-                        </Item>
-                    )}
-                />
-            }
+            <List
+                data={data}
+                keyExtractor={item => item.id}
+                ItemSeparatorComponent={() => (
+                    <SeparatorList/>
+                )}
+                ListEmptyComponent={() => (
+                    <ZeroProducts>
+                        Não foi possivel encontrar nenhum produto.
+                    </ZeroProducts>
+                )}
+                renderItem={({item}) => (
+                    <Item
+                        onPress={() => handleSelectCategory(item)}
+                    >
+                        <Content>
+                            <ImageProduct
+                                source={{uri: item.imageUrl}}
+                                resizeMode="cover"
+                            />
+                            
+                            <Fields>
+                                <SuperiorFields>
+                                    <Title>
+                                        {item.title}
+                                    </Title>
+                                </SuperiorFields>
+
+                                <MiddleFields>
+                                    <SubTitle>
+                                        {StringFormat.substring(item.description, 50)}
+                                    </SubTitle>
+                                </MiddleFields>
+
+                                <InferiorFields>
+                                    <PriceField>
+                                        <IconPrice/>
+                                        <Price>
+                                            {item.price}
+                                        </Price>
+                                    </PriceField>
+                                </InferiorFields>
+                            </Fields>
+                        </Content>
+                    </Item>
+                )}
+            />
         </Container>
     );
 }
